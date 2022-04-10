6661e166325e51ca9001f0e981fd2d912e0cd8bc
Dawson95 <b.dursun@it-students.fr>
2022-04-10 14:04:45 +0200

    getSet Animal

--- models/animal.model.php
+++ models/animal.model.php
@@ -8,7 +8,7 @@ abstract class Animal{
     private $entryDate;
     private $gender;
     private $idParent;
-    private $photo;
+    private $photo = "default";
     private $diet;
     private $treatment;
     private $environnement;
@@ -26,5 +26,30 @@ abstract class Animal{
     public function getSpecies(){return $this->species;}
     public function setSpecies($species){$this->species = $species;}
 
+    public function getEntryDate(){return $this->entryDate;}
+    public function setEntryDate($entryDate){$this->entryDate = $entryDate;}
 
+    public function getGender(){return $this->gender;}
+    public function setGender($gender){$this->gender = $gender;}
+
+    public function getIdParent(){return $this->idParent;}
+    public function setIdParent($idParent){$this->idParent = $idParent;}
+
+    public function getPhoto(){return $this->photo;}
+    public function setPhoto($photo){$this->photo = $photo;}
+
+    public function getDiet(){return $this->diet;}
+    public function setDiet($diet){$this->diet = $diet;}
+
+    public function getTreatment(){return $this->treatment;}
+    public function setTreatment($treatment){$this->treatment = $treatment;}
+
+    public function getEnvironnement(){return $this->environnement;}
+    public function setEnvironnement($environnement){$this->environnement = $environnement;}
+
+    public function getDeathDate(){return $this->deathDate;}
+    public function setDeathDate($deathDate){$this->deathDate = $deathDate;}
+
+    public function getInformation(){return $this->information;}
+    public function setInformation($information){$this->information = $information;}
 }
